6755a955ff47855a1b0a3a4530bf9c2f4e0427ac
laninavi <60703893+laninavi@users.noreply.github.com>
2020-02-07 22:21:30 +0700

    Exercise4_Asoke_Y.py
    ส่งการบ้านครับ

---
+++ Exercise4_Asoke_Y.py
@@ -0,0 +1,8 @@
+FEng = 75
+GenBus = 54
+ICS = 97
+Cprog = 79
+print("Foundation English : ",FEng,"Point")
+print("General Business : ",GenBus,"Point")
+print("Introduction to Computer Systems : ",ICS,"Point")
+print("Computer Programing :", Cprog,"Point")
